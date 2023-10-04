d715a54372f239a1808e78995b77f1322484b50d
rvmeesa <130269585+rvmeesa@users.noreply.github.com>
2023-10-04 20:59:36 +0500

    Time: 180 ms (74.33%), Space: 17.1 MB (45.96%) - LeetHub

---
+++ 0817-design-hashmap/0817-design-hashmap.py
@@ -0,0 +1,35 @@
+class MyHashMap:
+    def __init__(self):
+        self.capacity = 10000
+        self.table = [None] * self.capacity
+
+    def hash(self, key):
+        return key % self.capacity
+
+    def put(self, key, value):
+        index = self.hash(key)
+        if self.table[index] is None:
+            self.table[index] = [(key, value)]
+        else:
+            for i, (k, v) in enumerate(self.table[index]):
+                if k == key:
+                    self.table[index][i] = (key, value)
+                    return
+            self.table[index].append((key, value))
+
+    def get(self, key):
+        index = self.hash(key)
+        if self.table[index] is not None:
+            for k, v in self.table[index]:
+                if k == key:
+                    return v
+        return -1
+
+    def remove(self, key):
+        index = self.hash(key)
+        if self.table[index] is not None:
+            for i, (k, v) in enumerate(self.table[index]):
+                if k == key:
+                    del self.table[index][i]
+                    return
+
